15845136aab8dc235a6aa31a73b6e9a72d42ea8d
Evgeniy Sokolov <ewgraf@gmail.com>
2016-01-19 13:50:18 +0100

    fix typos

--- files/.travis.sh
+++ files/.travis.sh
@@ -5,22 +5,25 @@ EXIT_CODE=0
 
 go build -o $DIR/spell-checker $DIR/spell-checker.go
 
+cat $DIR/dictionary.dic | wc -l > $DIR/dictionary_custom.dic
+cat $DIR/dictionary.dic | sort >> $DIR/dictionary_custom.dic
+
 git diff HEAD^ --name-status | grep "^D" -v | awk '{print $2}' > changed_files
 
 while read FILE; do
     echo -n "Проверка файла $FILE на опечатки... ";
 
-    OUTPUT_RU=$(cat "$FILE" | hunspell -d russian-aot,en_US,de_DE,$DIR/dictionary | $DIR/spell-checker);
+    OUTPUT_RU=$(cat "$FILE" | sed 's/https\?:[^ ]*//g' | sed "s/[(][^)]*\.md[)]//g" | hunspell -d russian-aot,ru_RU,en_US,de_DE,$DIR/dictionary_custom | $DIR/spell-checker);
     RU_EXIT_CODE=$?
-    OUTPUT_EN=$(cat "$FILE" | hunspell -d en_US,russian-aot,de_DE,$DIR/dictionary | $DIR/spell-checker);
+    OUTPUT_EN=$(cat "$FILE" | sed 's/https\?:[^ ]*//g' | sed "s/[(][^)]*\.md[)]//g" | hunspell -d en_US,russian-aot,ru_RU,de_DE,$DIR/dictionary_custom | $DIR/spell-checker);
     EN_EXIT_CODE=$?
-    OUTPUT_DE=$(cat "$FILE" | hunspell -d de_DE,en_US,russian-aot,$DIR/dictionary | $DIR/spell-checker);
+    OUTPUT_DE=$(cat "$FILE" | sed 's/https\?:[^ ]*//g' | sed "s/[(][^)]*\.md[)]//g" | hunspell -d de_DE,en_US,russian-aot,ru_RU,$DIR/dictionary_custom | $DIR/spell-checker);
     DE_EXIT_CODE=$?
 
     if [ $RU_EXIT_CODE -ne 0 ] || [ $EN_EXIT_CODE -ne 0 ] || [ $DE_EXIT_CODE -ne 0 ]; then
         EXIT_CODE=1;
         echo "ошибка";
-        echo "$OUTPUT_RU\n$OUTPUT_EN\n$OUTPUT_DE" | sort -n -k2 | uniq;
+        echo "$OUTPUT_RU\n$OUTPUT_DE\n$OUTPUT_EN" | sort -n -k2 | uniq;
     else
         echo "пройдена";
     fi

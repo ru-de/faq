28354c129dbdf72d28f609685ff707198059abdf
Evgeniy Sokolov <e.sokolov@sevensenders.com>
2018-01-02 19:35:29 +0100

    update.sh fix

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ rm -rf $DE_FAQ_DIR
 git clone https://github.com/ru-de/faq.git $DE_FAQ_DIR --depth=1
 
 rm $DE_FAQ_DIR/CONTRIBUTING.md
+
+(echo "---\nlayout: default\n---\n"; cat "$DE_FAQ_DIR/README.md") > index.md
 rm $DE_FAQ_DIR/README.md
 
 mkdir $DE_FAQ_DIR/tmp_pages
@@ -12,14 +14,10 @@ mkdir $DE_FAQ_DIR/tmp_pages
 for file in $DE_FAQ_DIR/*.md; do
   bname=$(basename "$file")
   title=$(basename "$file" .md)
- (echo "---\ntitle: $title\nlayout: default\n---\n"; cat "$file") > "$DE_FAQ_DIR/tmp_pages/$bname"
+  (echo "---\ntitle: $title\nlayout: default\n---\n"; cat "$file") > "$DE_FAQ_DIR/tmp_pages/$bname"
 done
-
-sed -i -- 's/ewgRa\/de_faq/ru-de\/faq/g' $DE_FAQ_DIR/tmp_pages/*.md
 
 rm -rf pages
 mkdir pages
 cp -R $DE_FAQ_DIR/files pages
-sed -i -- 's/ewgRa\/de_faq/ru-de\/faq/g' pages/files/known_url.csv
-
 cp $DE_FAQ_DIR/tmp_pages/*.md pages

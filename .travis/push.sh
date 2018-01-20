91e1cb6756d03d3752d213825b4fb329f8720cd5
Andrey Orsoev <andrey.orsoev@gmail.com>
2018-01-20 15:40:51 +0100

    Moved shell scripts to .travis, added sort & push.sh on success

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git checkout master
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-master https://${GH_TOKEN}@github.com/andreyors/de_faq.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-master master
+}
+
+setup_git
+commit_files
+upload_files

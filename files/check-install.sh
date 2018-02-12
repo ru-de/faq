cd19c0a6ebe2b87fc1011f69195d22bb6b68a491
Andrey O <andreyors@users.noreply.github.com>
2018-02-12 22:50:49 +0100

    Немного автоматизации для сортировки словаря (#343)
    * Update dictionary.dic
    
    * Added secret token
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Moved shell scripts to .travis, added sort & push.sh on success
    
    * Travis build: 4
    
    * Fixed dir issue
    
    * Excluded build-related files
    
    * Sort if changed only
    
    * Travis build: 7
    
    * Travis build: 9
    
    * Set locale
    
    * Moved back travis scripts
    
    * Fixed travis
    
    * Changed files dir back
    
    * Reorganized travisci
    
    * Added git clone depth and skipping submodules
    
    * Extracted run_on_master.sh
    
    * Fixed cs

--- files/check-install.sh
+++ files/check-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -e
+set -xe
 
-apt-get -qq update
-apt-get install -y hunspell hunspell-ru hunspell-en-us hunspell-de-de
+apt-get -yqq update && apt-get install -y hunspell hunspell-ru hunspell-en-us hunspell-de-de
 curl -s https://extensions.libreoffice.org/extensions/russian-spellcheck-dictionary.-based-on-works-of-aot-group > .dict_page
 cat .dict_page | grep -oP "<a href.+title=\"Current release for the project\"" | grep -oP "https://extensions.libreoffice.org/extensions/russian-spellcheck-dictionary.-based-on-works-of-aot-group/[^\"]+" > .current_release
 echo -n $(cat .current_release) > .current_release
---
+++ files/run_on_master.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+skip() {
+	echo "$@" 1>&2
+	echo "Exiting..." 1>&2
+	exit 0
+}
+
+[ "${TRAVIS_PULL_REQUEST}" = "false" ] || \
+    skip "Not running master-only script for pull-requests."
+
+[ "${TRAVIS_BRANCH}" = "master" ] || \
+    skip "Running master-only for updates on 'master' branch (current: ${TRAVIS_BRANCH})."
+
+[ "${TRAVIS_REPO_SLUG}" = "ru-de/faq" ] || \
+    skip "Running master-only for updater on main repo (current: ${TRAVIS_REPO_SLUG})."
+
+[ "${GH_TOKEN+set}" = set ] || \
+    skip "GitHub access token not available, skipping dict check."
+
+dict_check() {
+    LC_ALL=ru_RU.UTF8 sort files/dictionary.dic -C || \
+    (LC_ALL=ru_RU.UTF8 sort files/dictionary.dic -o files/dictionary.dic -f && sh files/push.sh)
+}
+
+dict_check

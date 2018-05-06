d96610086136a85cf1147de9c2cd879bf7419af6
Evgeniy Sokolov <ewgraf@gmail.com>
2018-05-06 02:27:28 +0200

    Inline comments (#381)
    Inline comments from ci

--- files/check-install.sh
+++ files/check-install.sh
@@ -2,7 +2,9 @@
 
 set -xe
 
-apt-get -yqq update && apt-get install -y hunspell hunspell-ru hunspell-en-us hunspell-de-de
+DIR=`dirname $0`
+
+apt-get -yqq update && apt-get install -y hunspell hunspell-ru hunspell-en-us hunspell-de-de jq
 curl -s https://extensions.libreoffice.org/extensions/russian-spellcheck-dictionary.-based-on-works-of-aot-group > .dict_page
 cat .dict_page | grep -oP "<a href.+title=\"Current release for the project\"" | grep -oP "https://extensions.libreoffice.org/extensions/russian-spellcheck-dictionary.-based-on-works-of-aot-group/[^\"]+" > .current_release
 echo -n $(cat .current_release) > .current_release
@@ -27,4 +29,17 @@ for dic in $dicList
         cat /usr/share/hunspell/$dic.aff | iconv --from ISO8859-1 --to UTF-8 | sed 's/SET ISO8859-1/SET UTF-8/' > /usr/share/hunspell/$dic-utf8.aff
 done
 
-go get -u github.com/russross/blackfriday-tool
+go get ./...
+go get -u github.com/ewgRa/ci-utils/utils/diff_liner
+go get -u github.com/ewgRa/ci-utils/utils/hunspell_parser
+go get -u github.com/ewgRa/ci-utils/utils/github_comments_diff
+go get -u github.com/ewgRa/ci-utils/utils/github_comments_send
+
+go build -o /tmp/check_spell $DIR/go/check_spell/main.go
+go build -o /tmp/check_links $DIR/go/check_links/main.go
+
+(cat $DIR/dictionary.dic; echo) | sed '/^$/d' | wc -l > /tmp/dictionary.dic
+(cat $DIR/dictionary.dic; echo) | sed '/^$/d' >> /tmp/dictionary.dic
+
+echo "SET UTF-8" >> /tmp/dictionary.aff
+sudo mv /tmp/dictionary.* /usr/share/hunspell

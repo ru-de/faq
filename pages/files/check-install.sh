75e6108f9f3ef5a24cad0885b674af225dd2172b
Travis CI <travis@travis-ci.org>
2020-10-15 19:37:57 +0000

    Travis #1385: sync github pages

--- pages/files/check-install.sh
+++ pages/files/check-install.sh
@@ -5,11 +5,10 @@ set -xe
 DIR=`dirname $0`
 
 apt-get -yqq update && apt-get install -y hunspell hunspell-ru hunspell-en-us hunspell-de-de jq
-curl -s https://extensions.libreoffice.org/extensions/russian-spellcheck-dictionary.-based-on-works-of-aot-group > .dict_page
-cat .dict_page | grep -oP "<a href.+title=\"Current release for the project\"" | grep -oP "https://extensions.libreoffice.org/extensions/russian-spellcheck-dictionary.-based-on-works-of-aot-group/[^\"]+" > .current_release
-echo -n $(cat .current_release) > .current_release
-echo -n "/@@download[^\"]+" >> .current_release
-cat .dict_page | grep -oP -f .current_release | wget -q -i - -O /tmp/dictionary.otx
+curl -s https://extensions.libreoffice.org/en/extensions/show/russian-spellcheck-dictionary > .dict_page
+echo -n "https://extensions.libreoffice.org" > .current_release
+strings .dict_page | grep -ozP '<li class="releaseRow">(\n|.)*?</li>' | grep -zoP 'href=".*?">Download' | head -1 | sed 's/href="//' | sed 's/">Download//' >> .current_release
+cat .current_release | wget -q -i - -O /tmp/dictionary.otx
 unzip /tmp/dictionary.otx -d /tmp
 cp /tmp/*.dic /usr/share/hunspell
 cp /tmp/*.aff /usr/share/hunspell

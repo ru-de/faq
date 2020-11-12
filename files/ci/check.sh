6bdd5fae9422f97cb2237f8e880ae4304a37c4d2
Evgenii Sokolov <ewgraf@gmail.com>
2020-11-12 20:11:15 +0100

    try this env

--- files/ci/check.sh
+++ files/ci/check.sh
@@ -4,7 +4,9 @@ DIR=`dirname $0`
 
 git config --global core.quotepath false
 
-curl "https://github-api-bot.herokuapp.com/diff?repo=${github.repository}&pr=${github.event.number}" > /tmp/pr.diff
+PULL_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+curl "https://github-api-bot.herokuapp.com/diff?repo=${GITHUB_REPOSITORY}&pr=${PULL_NUMBER}" > /tmp/pr.diff
 
 if [ "$?" != "0" ]; then
     echo "Can't get github pull request diff, probably rate limit? Try to restart CI build"
@@ -16,7 +18,7 @@ cat /tmp/pr.diff | diff_liner > /tmp/pr_liner.json
 rm -f /tmp/comments.json
 touch /tmp/comments.json
 
-curl -s -X GET -G https://api.github.com/repos/${github.repository}/pulls/${github.event.number}/files | jq -r '.[] | .filename' | grep "^D" -v | sed 's/^.\t//g' | grep "\.md$" > /tmp/changed_files
+curl -s -X GET -G https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PULL_NUMBER}/files | jq -r '.[] | .filename' | grep "^D" -v | sed 's/^.\t//g' | grep "\.md$" > /tmp/changed_files
 
 while read FILE; do
     COMMIT=$(git log --pretty=format:"%H" -1 "$FILE");
@@ -40,7 +42,7 @@ cat /tmp/comments_array.json
 EXIT_CODE=0
 
 if [ "$(cat /tmp/comments_array.json)" != "[]" ]; then
-    curl "https://github-api-bot.herokuapp.com/comments?repo=${github.repository}&pr=${github.event.number}" > /tmp/pr_comments.json
+    curl "https://github-api-bot.herokuapp.com/comments?repo=${GITHUB_REPOSITORY}&pr=${PULL_NUMBER}" > /tmp/pr_comments.json
 
     if [ "$?" != "0" ]; then
         echo "Can't get github comments, probably rate limit? Try to restart ci build"
@@ -49,7 +51,7 @@ if [ "$(cat /tmp/comments_array.json)" != "[]" ]; then
 
     github_comments_diff -comments /tmp/comments_array.json -exists-comments /tmp/pr_comments.json > /tmp/send_comments.json
 
-    curl -XPOST "https://github-api-bot.herokuapp.com/send_review?repo=${github.repository}&pr=${github.event.number}&body=Спасибо%20за%20PR.%20Обратите%20внимание%20на%20результаты%20автоматической%20проверки%20орфографии%20и%20ссылок" -d @/tmp/send_comments.json
+    curl -XPOST "https://github-api-bot.herokuapp.com/send_review?repo=${GITHUB_REPOSITORY}&pr=${PULL_NUMBER}&body=Спасибо%20за%20PR.%20Обратите%20внимание%20на%20результаты%20автоматической%20проверки%20орфографии%20и%20ссылок" -d @/tmp/send_comments.json
 
     EXIT_CODE=1
 fi

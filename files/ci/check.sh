47909a940b9262f060df17efc1f941700fb19a53
Evgenii Sokolov <ewgraf@gmail.com>
2020-11-12 19:41:13 +0100

    add check

--- files/ci/check.sh
+++ files/ci/check.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo "Skip, because not a PR"
-    exit 0
-fi
-
 DIR=`dirname $0`
 
 git config --global core.quotepath false
 
 git diff HEAD^ --name-status | grep "^D" -v | sed 's/^.\t//g' | grep "\.md$" > /tmp/changed_files
 
-curl "https://github-api-bot.herokuapp.com/diff?repo=$TRAVIS_REPO_SLUG&pr=$TRAVIS_PULL_REQUEST" > /tmp/pr.diff
+curl "https://github-api-bot.herokuapp.com/diff?repo=${github.repository}&pr=${github.event.number}" > /tmp/pr.diff
 
 if [ "$?" != "0" ]; then
     echo "Can't get github pull request diff, probably rate limit? Try to restart CI build"
@@ -45,7 +40,7 @@ cat /tmp/comments_array.json
 EXIT_CODE=0
 
 if [ "$(cat /tmp/comments_array.json)" != "[]" ]; then
-    curl "https://github-api-bot.herokuapp.com/comments?repo=$TRAVIS_REPO_SLUG&pr=$TRAVIS_PULL_REQUEST" > /tmp/pr_comments.json
+    curl "https://github-api-bot.herokuapp.com/comments?repo=${github.repository}&pr=${github.event.number}" > /tmp/pr_comments.json
 
     if [ "$?" != "0" ]; then
         echo "Can't get github comments, probably rate limit? Try to restart ci build"
@@ -54,7 +49,7 @@ if [ "$(cat /tmp/comments_array.json)" != "[]" ]; then
 
     github_comments_diff -comments /tmp/comments_array.json -exists-comments /tmp/pr_comments.json > /tmp/send_comments.json
 
-    curl -XPOST "https://github-api-bot.herokuapp.com/send_review?repo=$TRAVIS_REPO_SLUG&pr=$TRAVIS_PULL_REQUEST&body=Спасибо%20за%20PR.%20Обратите%20внимание%20на%20результаты%20автоматической%20проверки%20орфографии%20и%20ссылок" -d @/tmp/send_comments.json
+    curl -XPOST "https://github-api-bot.herokuapp.com/send_review?repo=${github.repository}&pr=${github.event.number}&body=Спасибо%20за%20PR.%20Обратите%20внимание%20на%20результаты%20автоматической%20проверки%20орфографии%20и%20ссылок" -d @/tmp/send_comments.json
 
     EXIT_CODE=1
 fi
